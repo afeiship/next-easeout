dd5622e0b1ebce3180d71e7c9783cf77dace02af
afeiship <1290657123@qq.com>
2018-09-21 23:30:31 +0800

    issue: remove polyfilll

--- README.md
+++ README.md
@@ -6,5 +6,15 @@
 npm install -S afeiship/next-easeout --registry=https://registry.npm.taobao.org
 ```
 
+## polyfill if need:
+```js
+// polyfill:
+if (!window.requestAnimationFrame) {
+  window.requestAnimationFrame = function (inCallback) {
+    window.setTimeout(inCallback, 17);
+  };
+}
+```
+
 ## resources:
 + https://www.zhangxinxu.com/wordpress/2017/01/share-a-animation-algorithm-js/
--- src/next-easeout.js
+++ src/next-easeout.js
@@ -3,13 +3,6 @@
   var global = global || this || window || Function('return this')();
   var nx = global.nx || require('next-js-core2');
 
-  // polyfill:
-  if (!window.requestAnimationFrame) {
-    window.requestAnimationFrame = function (inCallback) {
-      window.setTimeout(inCallback, 17);
-    };
-  }
-
   nx.easeout = function (inFrom, inTo, inRate, inCallback) {
     if (inFrom === inTo) return;
     var rate = inRate || 2;
@@ -23,7 +16,7 @@
         return;
       }
       callback(inFrom, false);
-      window.requestAnimationFrame(step);
+      global.requestAnimationFrame(step);
 
     }());
   };
